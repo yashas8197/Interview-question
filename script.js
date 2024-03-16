332b33056c9b8af32b805eb72da1bf002b7a4422
yashas8197 <yashasjr7@gmail.com>
2024-03-16 13:16:30 +0530

    Capitaize 1 letter - 5

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-// --- Directions
-// Given an array and a chunk size, divide the array into many subarray. where each subarray is of each size 
-// --- Example
-// chunk([1,2,3,4],2) --> [[1,2],[3,4]]
-// chunk([1,2,3,4,5],2) --> [[1,2],[3,4],[5]]
-// chunk([1,2,3,4,5,6,7,8], 3) --> [[1,2,3],[4,5,6],[7,8]]
-// chuck([1,2,3,4,5],4) --> [[1,2,3,4],[5]]
-// chuck([1,2,3,4,5],10) --> [[1,2,3,4,5]]
+//Write a function that accepts the string. The function should capitalize the firat letter of each word in the string then return the capitalized string.
+// ---Example
+// capitalize('this is yashas') ---> This Is Yashas
+// capitalize('title of the books, movies, songs') ---> 'Title Of The Books, Movies, Songs
 
-//this is the only method
-function chunk(arr, size){
-    let chunks = [];
-    let index = 0;
+//using map
+const capitalize = (str) => str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1, word.length)).join(" ");
 
-    while(index < arr.length){
-        chunks.push(arr.slice(index, index + size))
-        index += size
+console.log(capitalize('title of the books, movies, songs'))
+
+//using for of
+
+function capitalized(str){
+    const words = str.split(' ')
+    let result = []
+    for(let word of words){
+        result.push(word[0].toUpperCase() + word.slice(1))
     }
-    return chunks;
-}
+    return result.join(' ')
+} 
 
-console.log(chunk([1,2,3,4,5],4))
+console.log(capitalized('this is yashas'))

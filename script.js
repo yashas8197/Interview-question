93655829acb818372195df8babc67d205cf04eab
yashas8197 <yashasjr7@gmail.com>
2024-03-16 13:12:06 +0530

    array chunk - 4

--- script.js
+++ script.js
@@ -1,48 +1,22 @@
-// --- Direction
-// Given a string, return the character that is most commonly usedd in the string
-// ---Example
-// maxChar("abccccccd") === "c"
-// maxChar("apple 1231111") === "1"
+// --- Directions
+// Given an array and a chunk size, divide the array into many subarray. where each subarray is of each size 
+// --- Example
+// chunk([1,2,3,4],2) --> [[1,2],[3,4]]
+// chunk([1,2,3,4,5],2) --> [[1,2],[3,4],[5]]
+// chunk([1,2,3,4,5,6,7,8], 3) --> [[1,2,3],[4,5,6],[7,8]]
+// chuck([1,2,3,4,5],4) --> [[1,2,3,4],[5]]
+// chuck([1,2,3,4,5],10) --> [[1,2,3,4,5]]
 
-function maxChar1(str){
-    const countObj = {}
-    for(let char of str){
-        countObj[char] = (countObj[char] + 1) || 1
-    }
+//this is the only method
+function chunk(arr, size){
+    let chunks = [];
+    let index = 0;
 
-    let maxCount = 0;
-    let maxChar = ''
-    for(let key in countObj){
-        if(countObj[key] > maxCount){
-            maxCount = countObj[key]
-            maxChar = key
-        }
+    while(index < arr.length){
+        chunks.push(arr.slice(index, index + size))
+        index += size
     }
-    return maxChar
+    return chunks;
 }
 
-console.log(maxChar1('apple 1231111'))
-
-function maxChar(str){
-    const charMap = {}
-    for(let char of str){
-        if(charMap[char]){
-            charMap[char] = charMap[char] + 1
-        }else{
-            charMap[char] = 1
-        }
-    }
-
-    let max = 0;
-    let maxChar = ''
-    // older way to loop though object by converting object to array
-    for(const [key, value] of Object.entries(charMap)){
-        if(value > max){
-            max = value;
-            maxChar = key;
-        }
-    }
-    return maxChar;
-}
-
-console.log(maxChar('apple 1231111'))
+console.log(chunk([1,2,3,4,5],4))

51280fb476f259c557a6c8fb1ddebca901cfd108
yashas8197 <yashasjr7@gmail.com>
2024-03-19 10:59:39 +0530

    fizz buzz - 7

--- script.js
+++ script.js
@@ -1,60 +1,27 @@
-// --- Directions
-// Write a function that returns the number of vowels used in a string. Vowels are the charecters 'a', 'e', 'i', 'o', 'u'.
-// ---Example
-// vowels('Hi There!') ---> 3
-// vowels('How are you?') ---> 5
-// vowels('Coding Money') ---> 4
-// vowels('why?') ---> 0
+// --- Direction
+// Write a program that console logs the numbers from 1 to n. But for multipy of 3, print "fizz" instead of number and for the multiplies of 5 print "buzz". For  numbers which are multiples of both 3 and 5 print "fizzbuzz".
+//--- Example
+// fizzbuzz(5);
+// 1
+// 2
+// fizz
+// 4
+// buzz
 
-function vowels1(str) {
-  const vowel = ["a", "e", "i", "o", "u"];
-  let countVowels = 0;
-  let strArray = str.split("");
-  for (let i = 0; i < strArray.length; i++) {
-    for (let j = 0; j < vowel.length; j++) {
-      if (strArray[i] === vowel[j]) {
-        ++countVowels;
-      }
+function fizzBuzz(n) {
+  const fizzBuzzArr = [];
+  for (let i = 1; i <= n; i++) {
+    if(i % 3 === 0 && i % 5 === 0 ){
+      fizzBuzzArr.push("fizzbuzz")
+    }else if(i % 5 === 0){
+      fizzBuzzArr.push("buzz")
+    }else if(i % 3 === 0){
+      fizzBuzzArr.push("fizz")
+    }else{
+      fizzBuzzArr.push(i)
     }
   }
-  return countVowels;
+  return fizzBuzzArr
 }
 
-console.log(vowels1("How are you?"));
-
-function vowels2(str) {
-  let strArray = str.toLowerCase().split("");
-
-  const filterVowelChar = strArray.filter(
-    (char) => char.includes("a") || char.includes("e") || char.includes('i') || char.includes('o') || char.includes('u')  
-  );
-  return filterVowelChar.length;
-}
-
-console.log(vowels2('I am yashas'));
-
-function vowels3(str) {
-  const vowel = ["a", "e", "i", "o", "u"];
-  let countVowels = 0;
-  let strArray = str.toLowerCase().split("");
-
-  const filterVowelChar = strArray.filter(
-    (char) => vowel.includes(char)  
-  );
-  return filterVowelChar.length;
-}
-
-console.log(vowels3('I am yashas'));
-
-function vowels4(str) {
-  const vowel = ["a", "e", "i", "o", "u"];
-  let count = 0;
-  let strArray = str.toLowerCase().split("");
-
-  for(let char of strArray){
-    if(vowel.includes(char)) count++
-  }
-  return count
-}
-
-console.log(vowels4('I am yashas'));
+console.log(fizzBuzz(20));

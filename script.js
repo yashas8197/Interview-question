abb8df80762dcd223d1cc7674fb0c07e810977d4
yashas8197 <yashasjr7@gmail.com>
2024-03-24 16:01:06 +0530

    second largest number - 9

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
-// --- Direction
-// Write a function that accepts a positve number N. The function should console log a step shape with N level using the # charecter. Make sure the step has spaces on teh right hand side
-// --- Example
-// steps(2)
-// '#'
-// '##'
+// RoadSideCoder - array
+// Ques 1 - Second Largest number
+// Given an array of size N, print second largest distinct element from array
 
-//steps(3)
-// '#'
-// '##'
-// '###'
+// Input: [12,35,1,10,34,1] --> output: 34
+// Input: [10, 5, 10] --> output: 5
 
-function steps(n) {
-  let line = ""
-  for (let i = 1; i <= n; i++) {
-    line += '#'
-    console.log(line)
+function secondLargestNum(arr) {
+  let largest = Number.NEGATIVE_INFINITY;
+  let secondLargest = Number.NEGATIVE_INFINITY;
+
+  for (let i = 0; i < arr.length; i++) {  // Time Complextity: o(n) 
+    if (arr[i] > largest) {
+      secondLargest = largest;
+      largest = arr[i];
+    } else if (arr[i] != largest && arr[i] > secondLargest) {
+      secondLargest = arr[i];
+    }
   }
-  
+  return secondLargest;
 }
 
-steps(4);
+console.log(secondLargestNum([12, 31, 1, 34, 10, 1]));
+console.log(secondLargestNum([10, 5, 10]));
+// space Complextity: o(1)
+
+
+// question 2 - Rotate Array by k
+// Given an integer array nums, rotate the array to the right by k steps, where k is non-negative
+
+// Input: nums = [1,2,3,4,5,6,7], k = 3 ---->>>> output: [5,6,7,1,2,3,4]
+// Input: nums = [-1, -100, 3, 99], k = 2 ---->>>> output: [3,99,-1,-100]
+
+// Explanation - 
+// [1,2,3,4,5,6,7] -> [7,1,2,3,4,5,6] -> [6,7,1,2,3,4,5] -> [5,6,7,1,2,3,4]
